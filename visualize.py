9edc24129c40a8a22d0d4f0cf1d6e3337ce9e00b
Flavien Mathieu <72614724+Mflavien01@users.noreply.github.com>
2026-03-18 22:26:08 +0100

    Add visualize.py for graph visualization functionality

---
+++ visualize.py
@@ -0,0 +1,23 @@
+import json
+from pyvis.network import Network
+
+def visualize_graph(graph_data, output_file):
+    # print(graph_data[0])
+    net = Network(directed=True)
+    for i in range(len(graph_data)):
+        for triplet in graph_data[i]['triples']:
+            sujet = triplet['sub']
+            relation = triplet['rel']
+            objet = triplet['obj']
+            net.add_node(sujet, label=sujet)
+            net.add_node(objet, label=objet)
+            net.add_edge(sujet, objet, title=relation)
+    net.save_graph(output_file)
+        
+        
+        
+file="data/Text2KGBench_LettrIA/airport/ground_truth.jsonl"
+with open(file, 'r') as f:
+    graph_data = [json.loads(line) for line in f]
+visualize_graph(graph_data, 'graph.html')
+
